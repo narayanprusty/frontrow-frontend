01605e13ab2a024304fe760df2f8cb2a0ab82ab9
ItachiUchiha1998 <vinayak.shrivastava@gmail.com>
2018-11-28 05:06:28 +0530

    react setup with tabular done

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+   constructor(props){
+    super(props);
+    this.state = {
+      
+    };
+  }  
+
+  render() {
+    console.log(this.state);
+  
+  return (
+      <div>
+        HOME
+      </div> 
+   );
+  }
+}
+
+export default App;
---
+++ src/client-config.js
@@ -0,0 +1,3 @@
+let config={};
+config.BASE_API="http://localhost:7000"
+module.exports=config;
