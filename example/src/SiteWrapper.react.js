082254a305743a0d5c146274f9c2ac6c16e200ad
Utkarsh Mehta <utkarsh.mehta678@live.com>
2018-12-21 02:15:20 +0530

    Getting user profile 4 seconds after login

--- example/src/SiteWrapper.react.js
+++ example/src/SiteWrapper.react.js
@@ -268,42 +268,51 @@ class SiteWrapper extends React.Component<Props, State> {
     await this.setState({ address: add[0] });
   };
 
-  Login = async () => {
-    await this.Init();
+    Login = async () => {
+        await this.Init();
 
-    if (!window.web3) {
-      return alert("Please install Metamask!");
-    }
-    if (!web3) {
-      web3 = new Web3(window.web3.currentProvider);
-    }
+        if (!window.web3) {
+            return alert("Please install Metamask!");
+        }
+        if (!web3) {
+            web3 = new Web3(window.web3.currentProvider);
+        }
 
-    this.setState({ loading: true });
+        this.setState({ loading: true });
 
-    try {
-      var nonce = "";
-      var response = await fetch(
-        `http://localhost:7000/users?publicAddress=${add[0]}`
-      );
-      var data = await response.json();
-      if (data.users.length > 0) {
-        nonce = await data.users[0].nonce;
-      } else {
-        nonce = await this.handleSignup(add[0]);
-        nonce = await nonce.nonce;
-      }
-      console.log("nonce", nonce);
+        try {
+            var nonce = "";
+            var response = await fetch(
+                `http://localhost:7000/users?publicAddress=${add[0]}`
+            );
+            var data = await response.json();
+            if (data.users.length > 0) {
+                nonce = await data.users[0].nonce;
+            } else {
+                nonce = await this.handleSignup(add[0]);
+                nonce = await nonce.nonce;
+            }
+            console.log("nonce", nonce);
 
-      var sig = await this.handleSignMessage(add[0], nonce);
-      var token = await this.handleAuthenticate(add[0], sig);
-      var auth = await token.token;
-      var login = await this.handleLoggedIn(auth);
-      await this.setState({ loading: false });
-    } catch (e) {
-      window.alert(e);
-      this.setState({ loading: false });
-    }
-  };
+            var sig = await this.handleSignMessage(add[0], nonce);
+            var token = await this.handleAuthenticate(add[0], sig);
+            var auth = await token.token;
+            var login = await this.handleLoggedIn(auth);
+            await this.setState({ loading: false });
+
+            setTimeout(
+                function () {
+                    this.getuser(add[0]);
+                }
+                    .bind(this),
+                4000
+            );
+            
+        } catch (e) {
+            window.alert(e);
+            this.setState({ loading: false });
+        }
+    };
 
   render(): React.Node {
     const notificationsObjects = this.state.notificationsObjects || [];
