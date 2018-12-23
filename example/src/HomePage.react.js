dff05be245862f00145b9cde3c4c05208d041f81
Utkarsh Mehta <utkarsh.mehta678@live.com>
2018-12-24 04:20:20 +0530

    Loader while video loads up

--- example/src/HomePage.react.js
+++ example/src/HomePage.react.js
@@ -6,105 +6,109 @@ import jwtDecode from "jwt-decode";
 import SiteWrapper from "./SiteWrapper.react";
 import Moment from "react-moment";
 import { Redirect } from "react-router";
+import Loader from 'react-loader-spinner'
 
 const LS_KEY = "frontrow";
 
 class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: false,
-      auth: localStorage.getItem(LS_KEY) || undefined,
-      username: "",
-      send: "",
-      age: "",
-      location: "",
-      interests: [],
-      ok: false,
-      videos: []
-    };
-    this.hideAlert = this.hideAlert.bind(this);
-    this.VideoRead = this.VideoRead.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: false,
+            auth: localStorage.getItem(LS_KEY) || undefined,
+            username: "",
+            send: "",
+            age: "",
+            location: "",
+            interests: [],
+            ok: false,
+            videos: [],
+            loadedVideos: false,
+        };
+        this.hideAlert = this.hideAlert.bind(this);
+        this.VideoRead = this.VideoRead.bind(this);
+    }
 
-  VideoRead() {
-    fetch("http://localhost:7000/video/get/", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        authorization: localStorage.getItem("jwt")
-      }
-    })
-      .then(response => {
-        return response.json();
-      })
-      .then(json => {
-        this.setState({ videos: json.data });
-      });
-  }
-
-  componentWillMount() {
-    var accesstoken = this.state.auth;
-    this.VideoRead();
-    if (accesstoken == undefined) return;
-    accesstoken = accesstoken.replace(/\"/g, "");
-    const {
-      payload: { id }
-    } = jwtDecode(accesstoken);
-    fetch(`http://localhost:7000/users/${id}`, {
-      headers: {
-        Authorization: `Bearer ${accesstoken}`
-      }
-    })
-      .then(response => response.json())
-      .then(json => {
-        let user = Object.assign({}, this.state.user);
-        user.publicAddress = json.data.publicAddress;
-        user.nonce = json.data.nonce;
-
-        fetch("http://localhost:7000/user/get/" + json.data.publicAddress, {
-          method: "POST",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-            authorization: localStorage.getItem("jwt")
-          }
+    VideoRead() {
+        this.setState({ loadedVideos: false });
+        fetch("http://localhost:7000/video/get/", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                authorization: localStorage.getItem("jwt")
+            }
         })
-          .then(response => {
-            return response.json();
-          })
-          .then(json => {
-            if (json.data[0] == undefined) return;
-            {
-              json.data[0].username == undefined
-                ? this.setState({ username: null })
-                : this.setState({ username: json.data[0].username });
-            }
-            {
-              json.data[0].age == undefined
-                ? this.setState({ age: null })
-                : this.setState({ age: json.data[0].age });
-            }
-            {
-              json.data[0].location == undefined
-                ? this.setState({ location: null })
-                : this.setState({ location: json.data[0].location });
-            }
-            {
-              json.data[0].interests == undefined
-                ? this.setState({ interests: null })
-                : this.setState({ interests: json.data[0].interests });
-            }
-            this.setState({ ok: true });
-          });
-      })
-      .catch(window.alert);
-  }
+            .then(response => {
+                return response.json();
+            })
+            .then(json => {
+                this.setState({ videos: json.data, loadedVideos: true
+                 });
+            });
+    }
 
-  hideAlert() {
-    this.setState({ send: "" });
-  }
+    componentWillMount() {
+        var accesstoken = this.state.auth;
+        this.VideoRead();
+        if (accesstoken == undefined) return;
+        accesstoken = accesstoken.replace(/\"/g, "");
+        const {
+            payload: { id }
+        } = jwtDecode(accesstoken);
+        fetch(`http://localhost:7000/users/${id}`, {
+            headers: {
+                Authorization: `Bearer ${accesstoken}`
+            }
+        })
+            .then(response => response.json())
+            .then(json => {
+                let user = Object.assign({}, this.state.user);
+                user.publicAddress = json.data.publicAddress;
+                user.nonce = json.data.nonce;
+
+                fetch("http://localhost:7000/user/get/" + json.data.publicAddress, {
+                    method: "POST",
+                    headers: {
+                        Accept: "application/json",
+                        "Content-Type": "application/json",
+                        authorization: localStorage.getItem("jwt")
+                    }
+                })
+                    .then(response => {
+                        return response.json();
+                    })
+                    .then(json => {
+                        if (json.data[0] == undefined) return;
+                        {
+                            json.data[0].username == undefined
+                                ? this.setState({ username: null })
+                                : this.setState({ username: json.data[0].username });
+                        }
+                        {
+                            json.data[0].age == undefined
+                                ? this.setState({ age: null })
+                                : this.setState({ age: json.data[0].age });
+                        }
+                        {
+                            json.data[0].location == undefined
+                                ? this.setState({ location: null })
+                                : this.setState({ location: json.data[0].location });
+                        }
+                        {
+                            json.data[0].interests == undefined
+                                ? this.setState({ interests: null })
+                                : this.setState({ interests: json.data[0].interests });
+                        }
+                        this.setState({ ok: true });
+                    });
+            })
+            .catch(window.alert);
+    }
+
+    hideAlert() {
+        this.setState({ send: "" });
+    }
 
   render() {
     //<GalleryCard.IconItem name="heart" label={} right />
@@ -145,7 +149,15 @@ class Home extends Component {
     return (
       <SiteWrapper>
         <Page.Content>
-          <Videos data={this.state.videos} />
+        {this.state.loadedVideos ? 
+            <Videos data={this.state.videos} /> :
+            <center><Loader 
+                type="Rings"
+                color="#ff002a"
+                height="100"	
+                width="100"
+            /></center>
+        }
         </Page.Content>
       </SiteWrapper>
     );
